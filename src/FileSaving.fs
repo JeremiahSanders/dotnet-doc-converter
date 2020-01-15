10f323a743bd7f697e0d918ed110d9f87e98368f
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2020-01-14 20:13:12 -0600

    Add output argument

--- src/FileSaving.fs
+++ src/FileSaving.fs
@@ -1,6 +1,17 @@
 namespace Jds.DotnetDocConverter
 
+open System.IO
+
 module FileSaving =
+    let private ensureDirectoryExists directory =
+        if Directory.Exists(directory) then directory
+        else
+            let _ = Directory.CreateDirectory(directory)
+            directory
+
+    let private getDirectory (path : string) = Path.GetDirectoryName(path)
+
     let saveFile path content =
-        System.IO.File.WriteAllText(path, content)
+        let _ = getDirectory path |> ensureDirectoryExists
+        File.WriteAllText(path, content)
         content
