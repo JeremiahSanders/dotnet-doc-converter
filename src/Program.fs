abc205d658c2fd4bcfb86c01f9fccb9465d5705e
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2020-01-17 16:26:26 -0600

    Improve CLI input parsing and output display

--- src/Program.fs
+++ src/Program.fs
@@ -7,13 +7,18 @@ open DocParsing
 open DocDisplay
 open FileLoading
 
+let generateAppHeader() = "Convert .NET XML documentation to Markdown\n\n"
+
 let generateUsage() =
     StringBuilder()
-    |> StringBuilder.appendLine "Dotnet Doc Converter"
+    |> StringBuilder.appendLine "Usage: dotnet docconvert <DOC_LOCATION> [options]"
     |> StringBuilder.appendLine ""
-    |> StringBuilder.appendLine "Usage:"
-    |> StringBuilder.appendLine "  [filePath]"
-    |> (fun stringBuilder -> stringBuilder.ToString())
+    |> StringBuilder.appendLine "Arguments:"
+    |> StringBuilder.appendLine "  <DOC_LOCATION>              Path to XML file or directory."
+    |> StringBuilder.appendLine ""
+    |> StringBuilder.appendLine "Options:"
+    |> StringBuilder.appendLine " -o, --output <DIRECTORY>     Path to save converted Markdown files."
+    |> StringBuilder.toString
 
 let generateDocs docFiles =
     docFiles
@@ -29,7 +34,8 @@ let generateDocs docFiles =
 let saveAssemblyDocument appArguments (assemblyDocument, markdown) =
     let fileName = assemblyDocument.Assembly.Name + ".md"
     let destinationPath = System.IO.Path.Combine(appArguments.OutputDirectory, fileName)
-    FileSaving.saveFile destinationPath markdown
+    FileSaving.saveFile destinationPath markdown |> ignore
+    destinationPath |> sprintf "Saved %s"
 
 let generateDisplay appArguments =
     if (List.isEmpty appArguments.DocFiles) then generateUsage()
@@ -42,7 +48,9 @@ let generateDisplay appArguments =
 let main argv =
     argv
     |> parseArguments
-    |> Option.map generateDisplay
-    |> Option.defaultWith generateUsage
-    |> printfn "%s"
+    |> function
+    | Success applicationArguments -> applicationArguments |> generateDisplay
+    | Failure reason ->
+        sprintf "Error: %s\n\n%s" reason (generateUsage())
+    |> printfn "%s%s" (generateAppHeader())
     0 // return an integer exit code
